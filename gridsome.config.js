91e556a7d3e360671280bdccc5744a2e255536fd
Akki Khare <admin@akki.io>
2020-11-07 00:52:32 -0500

    purgecss as per new tailwind config

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,13 +4,7 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const tailwind = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss");
-
-const postcssPlugins = [tailwind()];
-
-if (process.env.NODE_ENV === "production")
-  postcssPlugins.push(purgecss(require("./purgecss.config.js")));
+const postcssPlugins = [require("tailwindcss"), require("autoprefixer")];
 
 module.exports = {
   siteName: "Akshansh Khare",
--- src/main.js
+++ src/main.js
@@ -2,14 +2,9 @@ import DefaultLayout from "~/layouts/Default.vue";
 
 require("~/assets/css/styles.css");
 require("~/assets/css/github-markdown.css");
+require("typeface-nunito");
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
-
-  head.link.push({
-    rel: "stylesheet",
-    href:
-      "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700;800&display=swap"
-  });
 }
