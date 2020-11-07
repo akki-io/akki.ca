1e951a37d3b7a220c986fcfdcab9b266ab0067e1
Akki Khare <admin@akki.io>
2020-11-06 23:51:34 -0500

    aws font issue fix?

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import DefaultLayout from "~/layouts/Default.vue";
 
 require("~/assets/css/styles.css");
 require("~/assets/css/github-markdown.css");
+require("typeface-roboto");
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
